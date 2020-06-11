fd3536a4e62411bbf3b8a02d751e6e3fd5ee139d
Basaam Malik <basaammalik@gmail.com>
2020-06-11 22:50:01 +0000

    Complete test-driven development walkthrough.

--- walkthroughs/week-5-tdd/intro/src/test/java/com/google/sps/GreeterTest.java
+++ walkthroughs/week-5-tdd/intro/src/test/java/com/google/sps/GreeterTest.java
@@ -30,4 +30,24 @@ public final class GreeterTest {
 
     Assert.assertEquals("Hello Ada", greeting);
   }
+
+  @Test
+  public void testGreetingTrimsWhitespace() {
+    Greeter greeter = new Greeter();
+
+    String greeting = greeter.greet("   Ada   ");
+
+    // Whitespace should be trimmed.
+    Assert.assertEquals("Hello Ada", greeting);
+  }
+
+  @Test
+  public void testGreetingRemovesSymbols() {
+    Greeter greeter = new Greeter();
+
+    String greeting = greeter.greet("@A#$da#");
+
+    // Symbols should be removed.
+    Assert.assertEquals("Hello Ada", greeting);
+  }
 }
